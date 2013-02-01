fa31959dd3887ac872262e8e1a8f846598f84ef5
Andy Dufilie <andy.dufilie@gmail.com>
2013-02-01 18:52:37 -0500

    pain field renamed to Score and hacked to compute a sum of the first character in each custom field for an issue.

--- init.rb
+++ init.rb
@@ -5,11 +5,11 @@ require 'query_patch'
 require 'user_pain_issue_hook'
 
 Redmine::Plugin.register :redmine_redmine_user_pain do
-  name 'Redmine User Pain plugin'
-  author 'Théodore Biadala'
+  name 'Weave Voting Score (modified Redmine User Pain plugin)'
+  author 'Theodore Biadala, modified by Andy Dufilie'
   description 'Implement User Pain bug triage'
   version '0.0.1'
-  url 'http://theodoreb.net/redmine-user-pain-triage'
-  author_url 'http://theodoreb.net'
+  url 'https://github.com/adufilie/redmine_user_pain'
+  author_url 'https://github.com/adufilie'
 end
 
--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -13,32 +13,16 @@ module RedmineUserPain
     
     module InstanceMethods
       def user_pain
-        # get the last custom field
-        likelihood = CustomField.find_by_name("Likelihood")
-        likelihood_values_length = likelihood['possible_values'].length
-        pain = 1
-
-        # add likelihood value to total pain
-        self.custom_values.each do |x|
-          if x.custom_field_id == likelihood.id
-            pain *= likelihood_values_length - likelihood['possible_values'].index(x.value)
+        score = 0;
+        self.custom_values.each do |field|
+          begin
+            score += field.value[0] - '0'[0] # assumes value is a string that starts with a single digit
+            #score += field['possible_values'].index(field.value)
+          rescue
+            # ignore error
           end
         end
-
-        tracker_pain = 0
-        # get enabled tracker for the current project
-        @project.trackers.each do |t|
-          if self.tracker_id == t.id
-            pain *= @project.trackers.length - tracker_pain
-          end
-          tracker_pain += 1
-        end
-
-        # add type value to total pain
-        pain *= IssuePriority.all.length - (IssuePriority.find_by_id(self.priority_id).position - 1)
-
-        max_pain = @project.trackers.length * IssuePriority.all.length * likelihood_values_length
-        return 100 * pain / max_pain
+        return score
       end
 
       def css_classes_with_css_pain
