6ed86a780e5c9fc42ab2c015e500944296d8c99f
Andy Dufilie <andy.dufilie@gmail.com>
2013-02-04 13:40:28 -0500

    now only showing Score value on Vote issues

--- lib/user_pain_issue_hook.rb
+++ lib/user_pain_issue_hook.rb
@@ -7,8 +7,14 @@ class UserPainIssueHook  < Redmine::Hook::ViewListener
   # * :issue => Issue being rendered
   #
   def view_issues_show_details_bottom(context = { })
-    data = "<td><b>#{l(:user_pain)}:</b></td><td>#{context[:issue].user_pain}</td>"
-    return "<tr>#{data}<td></td></tr>"
+    begin
+      if "#{context[:issue].tracker}" == "Vote" then
+        data = "<td><b>#{l(:user_pain)}:</b></td><td>#{context[:issue].user_pain}</td>"
+        return "<tr>#{data}<td></td></tr>"
+      end
+    rescue Exception => e
+       return "user_pain_issue_hook.rb error: " + e.message
+    end
   end
 
 end
